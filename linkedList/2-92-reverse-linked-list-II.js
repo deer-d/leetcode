f1064c75bb645f8ee80857875d107f2058916feb
AfroHead <1932394099@qq.com>
2021-02-18 23:32:29 +0800

    linked node

--- linkedList/2-92-reverse-linked-list-II.js
+++ linkedList/2-92-reverse-linked-list-II.js
@@ -11,6 +11,20 @@
 //
 // 题目大意 #
 // 给定 2 个链表中结点的位置 m, n，反转这个两个位置区间内的所有结点
+// 思路：1、我们定义两个指针，分别称之为g(guard 守卫)和p(point)。
+// 我们首先根据方法的参数m确定g和p的位置。将g移动到第一个要反转的节点的前面，
+// 将p移动到第一个要反转的节点的位置上。我们以m=2，n=4为例。
+//
+    //   g
+    //     m     n
+    // D 2 4 6 8 2 1 4
+    //     p ->
+    //       r
+    //             
+
+    //      6 8 2 1
+    //      1 2 8 6
+
 
 
 // Definition for singly-linked list.
@@ -25,6 +39,7 @@ function ListNode(val, next) {
  * @param {number} n
  * @return {ListNode}
  */
+// 头插法
 var reverseBetween = function(head, m, n) {
     // 1.在head前插入虚拟节点
     let dummyHead = new ListNode(0);
@@ -32,7 +47,7 @@ var reverseBetween = function(head, m, n) {
     let g = dummyHead;
     let p = dummyHead.next;
 
-    // 2.移动到 m 位
+    // 2.将g(guard守卫)移动到 m 之前的一位， p移动到 m 位
     let step = 0;
     while (step < m - 1) {
         g = g.next;
@@ -42,18 +57,11 @@ var reverseBetween = function(head, m, n) {
 
     // 3.在m-n区间内反转
     for (let i = 0; i < n - m; i++) {
-        let temp = g.next
-
+        let removed = p.next // 取出 p 的后一位
+        p.next = p.next.next // p 指向后一位的后一位
+        removed.next = g.next; // 取出的 那一位 next 指向 g.next
+        g.next = removed // g.next 插入 removed
     }
-    //
-    //      g
-    //      m     n
-    //  2 4 6 8 2 1 4
-    //        p
-    //          r
-
-    //      6 8 2 1
-    //      1 2 8 6
 
     return dummyHead.next;
 };
--- linkedList/Node.js
+++ linkedList/Node.js
@@ -1,11 +1,39 @@
+// 链表的测试
+
 function ListNode(val, next) {
    this.val = (val===undefined ? 0 : val)
    this.next = (next===undefined ? null : next)
 }
 
-function createLinkedList(arr, len) {
-    if (!len) return null
+function createLinkedList(arr, n) {
+    if (n === 0) return null
 
-    while()
+    let head = arr[0]
+    let curNode = head
+    for(let i = 1; i < arr.length; i++) {
+        curNode.next = arr[i] // 创建新节点
+        curNode = curNode.next // 添加指向
+    }
+
+    return head
+
+}
+
+function printLinkedList(head) {
+    let curNode = head
+    while(curNode !== null) {
+        console.log(curNode.val + ' -> ')
+        curNode = curNode.next
+    }
+    console.log('null')
+}
+
+const __main = () => {
+    var arr = [1, 2, 3, 4, 5]
+    let n = arr.length
+
+    let list = createLinkedList(arr, n)
+    printLinkedList(list)
+    
 
 }
